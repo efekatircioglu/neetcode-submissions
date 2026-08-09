fab139c6b7902761e8b62e878f0cdee64caa995c
Efe Katircioglu <122750090+efekatircioglu@users.noreply.github.com>
2026-08-09 17:33:39 +0100

    Add: is-anagram - submission-9

---
+++ Data Structures & Algorithms/is-anagram/submission-9.py
@@ -0,0 +1,15 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        # hasmap for s:  letter to occurence
+        # another hasmap for t
+        if not s or not t:
+            return False
+        sMap = {}
+        tMap= {}
+
+
+        for letter in s:
+            sMap[letter]= 1+ sMap.get(letter,0)
+        for letter in t:
+            tMap[letter] = 1 + tMap.get(letter,0)        
+        return sMap == tMap
