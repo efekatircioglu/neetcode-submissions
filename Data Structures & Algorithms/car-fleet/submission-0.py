be6efb9be4f6f98220a6c6d8ac4f539c0ad10afe
Efe Katircioglu <122750090+efekatircioglu@users.noreply.github.com>
2026-09-04 11:54:58 +0100

    Add: car-fleet - submission-0

---
+++ Data Structures & Algorithms/car-fleet/submission-0.py
@@ -0,0 +1,17 @@
+class Solution:
+    def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
+        # input: target position, position array, speed array
+
+        pair=[(p,s)for p,s in zip(position,speed)]
+        pair.sort(key= lambda x:x[0], reverse=True )
+        stack=[]
+
+        for p,s in pair:
+            stack.append((target-p)/s)
+            # if more than 2 fleets, compare them:
+            # if current car's time <= car ahead : join fleet. else nothing
+            if len(stack) >=2 and stack[-1] <= stack[-2]:
+                stack.pop()
+        return len(stack)
+
+
